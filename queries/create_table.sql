bc3b5546f0cf543cc80c240f3c73de297e903cfb
Samuel Conde <samuelconra@192.168.0.122>
2025-10-19 21:36:59 -0600

    Improved Structured and Mail Added

--- queries/create_table.sql
+++ queries/create_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE Users (
     name VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    picture VARCHAR(255)
 );
