1eda84a1369e00d2ee754439f5daaa77771a2305
Samuel Conde <samuelconra@192.168.0.122>
2025-10-23 22:40:07 -0600

    queries folder deleted

--- queries/create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    picture VARCHAR(255)
-);
