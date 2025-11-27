430325d6b7403f75e18c9b4f513fdddf8e32b06a
Samuel Conde <samuelconra@gmail.com>
2025-11-26 20:00:09 -0600

    new feature

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -8,8 +8,16 @@ const getEmployees = async (req, res, next) => {
 
 const getEmployeeById = async (req, res, next) => {
     const { id } = req.params;
-    const employee = await EmployeeService.getById(id);
-    res.status(200).json({ employee });
+    const e = await EmployeeService.getById(id);
+    res.status(200).json({
+        id: e.id, role: e.role, first_name: e.first_name, last_name: e.last_name, email: e.email, 
+        phone_number: e.phone_number, address: e.address, birth_date: e.birth_date, image_url: e.image_url,
+        hire_date: e.hire_date, termination_date: e.termination_date, contract_type: e.contract_type, position: e.position,
+        department_id: e.department_id, supervisor_id: e.supervisor_id, status: e.status, reentry_date: e.reentry_date,
+        salary: e.salary, payroll_key: e.payroll_key, periodicity: e.periodicity, cost_center: e.cost_center,
+        vacation_days_total: e.vacation_days_total, vacation_days_taken: e.vacation_days_taken,
+        department_name: e.department_name, supervisor: `${e.supervisor_first_name} ${e.supervisor_last_name}`
+    });
 };
 
 const getEmployeeStats = async (req, res, next) => {
